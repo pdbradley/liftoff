61616d4c256b2b28f0bce68ca7bdf9d3db4bedbc
Philip Bradley <pdbradley@gmail.com>
2016-03-02 23:12:03 -0500

    updated views

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,6 +1,11 @@
 class Group < ActiveRecord::Base
   belongs_to :user
+  has_many :members
 
   validates :user, presence: true
   validates :name, presence: true
+
+  def num_members
+    members.size
+  end
 end
